62dbc0cb92ca3f25c2b577e4a823620c2d2ad564
Melissa <wolfaholic@users.noreply.github.com>
2022-10-02 21:38:54 -0700

    initial setup is complete.

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Header from './components/header/Header'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Experience from './components/experience/Experience'
+import Nav from './components/nav/Nav'
+import Portfolio from './components/portfolio/Portfolio'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Portfolio />
+        <Contact />
+        <Footer />
+
+    </>
+  )
+}
+
+export default App
