aceedb62bd92b174e49ad8fa00ed0e1c541c4c10
Melissa <wolfaholic@users.noreply.github.com>
2022-11-20 16:57:16 -0800

    updating title, cta image and cta background image

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # mmackie_portfolio
-Professional Portfolio for Melissa Mackie using React.JS, CSS and HTML. This portfolio was inspired by Egator, used to help cement my understanding of React, with adaptation for my own needs.
+Professional Portfolio for Melissa Mackie using React.JS, CSS and HTML. This portfolio was inspired with some code used by Egator's youtube video, used to help cement my understanding of React, with adaptation for my own needs.
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import Collab from '../../assets/selfimage1.png'
+import Collab from '../../assets/image_woman_2a.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Melissa Mackie</h1>
+        <h3>Hello, I'm</h3>
+        <h1 className="main_title">Melissa Mackie</h1>
         <h5 className="text-light">Full Stack Web Developer</h5>
         <CTA />
         <HeaderSocials />
