ce9f9e105246b79a70378b07bb9424e2b8d173b5
Melissa <wolfaholic@users.noreply.github.com>
2023-01-21 19:31:54 -0800

    updated css in about section, changed container color, font colors, added glassy background to content and picture to set apart from other sections

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
   return (
     <section id='about'>
       <h5>Get to Know</h5>
-      <h2>🔥 ABOUT ME 🔥</h2>
+      <h2 className="about__h2">🔥 ABOUT ME 🔥</h2>
       <div className="container about__container">
           <div className="about__me">
               <div className="about__me-image">
@@ -19,9 +19,9 @@ const About = () => {
               <p>    
               I have a very customer-service rich employment history with a sprinkling of health care and sales in-between. I have always sought and found a way to influence each position I have held with empathy, passion and a never give up mind-frame. Never give up means to me that even when you are at your lowest, you pause and allow yourself a moment, then you get back up and persist. This frame of mind has helped me get through some of the lowest points in my life. The one thing though I always wanted though is to really make a difference and to work in an industry where I can as well as have a creative outlet. After a sacrifice of time, money and lots of hard work. I have found it. I look forward to entering the tech industry.
               </p>
-              <p>☕️ Lets connect over Virtual Coffee (or Tea)!</p>
-              <div>
               <a href="#contact" className="btn btn-primary">Let's Talk over <BiCoffeeTogo className="header_icon" /></a>
+              <div>
+              
               </div>
               </article> 
           </div>
