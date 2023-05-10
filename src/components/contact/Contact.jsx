dd04a6959487dd180a5ce429016879213ceeda6f
Melissa Mackie <melissa.mackie@live.com>
2023-05-10 10:51:00 -0700

    getting rest of the portfolio in place, portfolio projects and contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className="contact__options">
+          <article className='contact_option'>
+            <h4>Email Me</h4>
+            <MdOutlineEmail />
+            <a href='mailto:melissa.mackie@live.com'>Send a Message</a>
+          </article>
+          <article className='contact_option'>
+            <h4>Email Me</h4>
+            <MdOutlineEmail />
+            <a href='mailto:melissa.mackie@live.com'>Send a Message</a>
+          </article>
+        </div>
+        {/* End of Contact Options*/}
+      </div>
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,61 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>portfolio</section>
+    <section id='portfolio'>
+        <h5>My Recent Work</h5>
+        <h2>Portfolio</h2>
+
+        <div className="container portfolio__container">
+          <article className='portfolio__item'>
+            <div className='portfolio__item-image'>
+              {/* <img src={IMG1} alt="" /> */}
+              <h3>Project Title</h3>
+              <a href="https://github.com" className='btn'>Github</a>
+              <a href="https://github.com" className='btn btn-primary' target='blank'>Live Demo</a>
+            </div>
+          </article>
+          <article className='portfolio__item'>
+            <div className='portfolio__item-image'>
+              {/* <img src={IMG1} alt="" /> */}
+              <h3>Project Title</h3>
+              <a href="https://github.com" className='btn'>Github</a>
+              <a href="https://github.com" className='btn btn-primary' target='blank'>Live Demo</a>
+            </div>
+          </article>
+          <article className='portfolio__item'>
+            <div className='portfolio__item-image'>
+              {/* <img src={IMG1} alt="" /> */}
+              <h3>Project Title</h3>
+              <a href="https://github.com" className='btn'>Github</a>
+              <a href="https://github.com" className='btn btn-primary' target='blank'>Live Demo</a>
+            </div>
+          </article>
+          <article className='portfolio__item'>
+            <div className='portfolio__item-image'>
+              {/* <img src={IMG1} alt="" /> */}
+              <h3>Project Title</h3>
+              <a href="https://github.com" className='btn'>Github</a>
+              <a href="https://github.com" className='btn btn-primary' target='blank'>Live Demo</a>
+            </div>
+          </article>
+          <article className='portfolio__item'>
+            <div className='portfolio__item-image'>
+              {/* <img src={IMG1} alt="" /> */}
+              <h3>Project Title</h3>
+              <a href="https://github.com" className='btn'>Github</a>
+              <a href="https://github.com" className='btn btn-primary' target='blank'>Live Demo</a>
+            </div>
+          </article>
+          <article className='portfolio__item'>
+            <div className='portfolio__item-image'>
+              {/* <img src={IMG1} alt="" /> */}
+              <h3>Project Title</h3>
+              <a href="https://github.com" className='btn'>Github</a>
+              <a href="https://github.com" className='btn btn-primary' target='blank'>Live Demo</a>
+            </div>
+          </article>
+        </div>
+    </section>
   )
 }
 
